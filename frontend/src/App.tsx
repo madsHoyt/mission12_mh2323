206cc7599df8867995c4cfeb659f46bfd58bb885
madHoyt <madisonhoyt19@gmail.com>
2023-03-21 23:13:41 -0600

    I hope I'm on my branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import data from './CollegeBasketballTeams.json';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Json stuff (delete later)
+//bring in json
 const schoolNames = data.teams;
 
 interface Team {
@@ -22,11 +21,13 @@ interface Team {
   longitude: number;
 }
 
+//calling functions
 function App() {
   return (
     <div>
       <Welcome/>
       <div className="container">
+        <h2>Schools</h2>
         <SchoolList/>
       </div>
       <div className='container'>
@@ -35,41 +36,26 @@ function App() {
     </div>
   );
 }
+//Welcome header--telling what the website is for
 function Welcome(){
   return(
     <div className="App">
       <header className="App-header">
-        <h1>NCAA Vasketball</h1>
+        <h1>NCAA Basketball</h1>
         <h2>Check out all the colleges in NCAA Basketball</h2>
-      </header>
-      <body>
-        <h2>Schools</h2>
-      </body>
-      
+      </header>      
     </div>
   )
 }
-/* class School extends React.Component{
-  render() {
-    const school = this.props;
-    return (
-      <div className="card">
-        <h3>{school.school}</h3>
-        <h4>Mascot: {school.mascot}</h4>
-        <h4>Mascot: {school.city}, {school.state}</h4>
-      </div>
-    );
-  }
 
-} */
-
+//Create a card for each school
 class School extends React.Component<{team: Team }>{
   render() {
     const team = this.props.team;
     return (
         <div>
           <div className="card">
-            <h3 className='schoolName'>School {team.school}</h3>
+            <h3 className='schoolName'>{team.school}</h3>
             <h4>Mascot: {team.name} </h4>
             <h4>Location: {team.city}, {team.state}</h4>
           </div>
@@ -88,6 +74,7 @@ function SchoolList()
   );
 } 
 
+//List all schools
 function AllSchools() {
   return (
     <body>
@@ -102,30 +89,6 @@ function AllSchools() {
     </body>
   );
 }
-
-/* function SchoolList()
-{
-  return (
-    <div>
-      {schoolNames[0].teams.map((team) => <School  team={team} key={team.tid}/>)}
-    </div>
-  );
-} 
-
-function AllSchools() {
-  return (
-    <div className='card'>
-      <h2>List of Schools</h2>
-      <ul>
-        {schoolNames[0].teams.map((team) => (
-          <li key={team.tid}>{team.school}</li>
-        ))}
-      </ul>
-    </div>
-  );
-} */
-
-
 
 /*ReactDOM.render(
   <App/>,
